9f4569e310e8abf2045402b1d71253d74f0fc173
Leonardo Candado <candadoleo@gmail.com>
2022-09-14 13:37:48 -0400

    Código Atualizado

---
+++ AppSorteio/app/src/main/java/cursoandroid/kotlin/com/br/MainActivity.kt
@@ -0,0 +1,21 @@
+package cursoandroid.kotlin.com.br
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sortearNumero(view: View){
+        var texto = findViewById<TextView>(R.id.textoSorteio)
+        var numeroSorteado = Random.nextInt(11)
+        texto.setText("Número sorteado é: " + numeroSorteado)
+
+    }
+}
